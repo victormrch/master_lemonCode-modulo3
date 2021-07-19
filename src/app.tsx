81a867e081aaea0373abc31dcb0015a368c54068
Victor Ramon <victormanuelchica@gmail.com>
2021-07-19 16:07:40 +0200

    Added logo and Welcome message in header

--- src/app.tsx
+++ src/app.tsx
@@ -5,20 +5,56 @@ import { Movie } from "./components/Movie";
 const FEATURE_API =
   "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=b766db798e47fc73c4d5e4e0e718c2d2&page1";
 
+const SEARCH_API =
+  "https://api.themoviedb.org/3/search/movie?api_key=b766db798e47fc73c4d5e4e0e718c2d2&query=";
+
+const userName: String = "Victor";
+
 export const App = () => {
   const [movies, setMovies] = React.useState<MovieEntity[]>([]);
+  const [searchTerm, setSearchTerm] = React.useState("");
 
-  React.useEffect(() => {
-    fetch(FEATURE_API)
+  const getMovies = (API: string) => {
+    fetch(API)
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
         setMovies(data.results);
       });
+  };
+
+  React.useEffect(() => {
+    getMovies(FEATURE_API);
   }, []);
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+
+    if (searchTerm) {
+      getMovies(SEARCH_API + searchTerm);
+
+      setSearchTerm("");
+    }
+  };
+
+  const handleOnChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
 
   return (
     <>
+      <header>
+        <img className="logo" src="./src/assets/logo.png" alt="icono" />
+        <h2>Welcome {userName} to BlockBuster!</h2>
+        <form onSubmit={handleOnSubmit}>
+          <input
+            className="search"
+            type="search"
+            placeholder="Search..."
+            value={searchTerm}
+            onChange={handleOnChange}
+          />
+        </form>
+      </header>
       <div className="movie-container">
         {movies.map((movie) => (
           <Movie key={movie.id} movie={movie} />
